2418402f90a2fb98aa5f46c9fa011ebd894f5eb0
freesinger <wzy545@vip.qq.com>
2018-11-12 22:26:25 +0800

    modified:   4_median_arrys.py

--- Algorithms/4_median_arrys.py
+++ Algorithms/4_median_arrys.py
@@ -15,7 +15,8 @@ The median is (2 + 3)/2 = 2.5
 """
 
 class Solution(object):
-    def findMedianOfArrays(self, str1, str2):
+    # 拼接之后排列，复杂度超出O(log (m+n))
+    def findMedianOfArrays_A(self, str1, str2):
         str1 = str1.split(',')
         # str1 = [n for n in str1.split(',')]
         str2 = [i for i in str2.split(',')]
@@ -27,9 +28,42 @@ class Solution(object):
         temp, num = sorted(str1), len(str1)
         return (float)((temp[0] + temp[num - 1]) / 2)
 
+    """
+    首先转成求A和B数组中第k小的数的问题, 然后用k/2在A和B中分别找。
+    """
+    def findMedainOfArrays_B(self, str1, str2):
+        def findKth(A, B, k):
+            if len(A) == 0:
+                return B[k - 1]
+            if len(B) == 0:
+                return A[k - 1]
+            if k == 1:
+                return min(A[0], B[0])
+            # 应该使用floor division
+            a = A[k // 2 - 1] if len(A) >= k // 2 else None
+            b = B[k // 2 - 1] if len(B) >= k // 2 else None
+
+            if b is None or (a is not None and a < b):
+                return findKth(A[k // 2:], B, k - k // 2)
+            return findKth(A, B[k // 2:], k - k // 2)
+        
+        str1 = str1.split(',')
+        str2 = str2.split(',')
+        str1 = list(map(int, str1))
+        str2 = list(map(int, str2))
+        n = len(str1) + len(str2)
+        
+        if n % 2 == 1:
+            return findKth(str1, str2, n // 2 + 1)
+        else:
+            smaller = findKth(str1, str2, n // 2)
+            bigger = findKth(str1, str2, n // 2 + 1)
+            return (smaller + bigger) / 2.0
+
 if __name__ == '__main__':
     # str_in_1 = input('Input 1st array: ').split(',')
     str_in_1 = input('Input 1st array: ')
     str_in_2 = input('Input 2nd Array: ')
     find = Solution()
-    print(find.findMedianOfArrays(str_in_1, str_in_2))
+    print(find.findMedianOfArrays_A(str_in_1, str_in_2))
+    # print(find.findMedainOfArrays_B(str_in_1, str_in_2))
