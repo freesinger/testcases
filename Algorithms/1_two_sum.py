ab5d6e557ac0943b1cccd6d9719437c2bfb72fc7
freesinger <wzy545@vip.qq.com>
2018-11-13 00:07:23 +0800

    modified:   1_two_sum.py

--- Algorithms/1_two_sum.py
+++ Algorithms/1_two_sum.py
@@ -8,21 +8,26 @@
 # Solutin A:
 
 class Solution(object):
-    def twoSum(self, nums, target):
+    def twoSum_A(self, nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
+                    print([i, j])
 
-#Solution B: (better)
+# Solution B: (better)
 
-class Solution(object):
-    def twoSum(self, nums, target):
+# class Solution(object):
+    def twoSum_B(self, nums, target):
         look_up = {}
         for i, num in enumerate(nums):
             if target - num in look_up:
-                return [look_up[target - num], i]
+                print([look_up[target - num], i])
             look_up[num] = i
-        return []
-    
+
+if __name__ == '__main__':
+    elements = input('Enter the elem: ').split(',')
+    elements = list(map(int, elements))
+    tar = int(input('Target num: '))
+    prog = Solution()
+    prog.twoSum_A(elements, tar)
+    prog.twoSum_B(elements, tar)
