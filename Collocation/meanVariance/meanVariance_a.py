4e7551a118aac1805a22baeb4cc2e10e4f4167e8
freesinger <wzy545@vip.qq.com>
2018-11-23 16:49:57 +0800

    modified:   meanVariance/meanVariance_a.py

--- Collocation/meanVariance/meanVariance_a.py
+++ Collocation/meanVariance/meanVariance_a.py
@@ -3,10 +3,10 @@ import os
 rootdir = '/Users/shanewang/Desktop/train/'
 resdir = '/Users/shanewang/Desktop/results/'
 # store result of frequences of nebor pairs
-
+dict_pair = {}
 # store results
 dict_res = {}
-
+doc = list(os.listdir(rootdir))
 
 def text_process(text, dist):
     single_npair = []
@@ -18,11 +18,18 @@ def text_process(text, dist):
                 temp = word.split('/')
                 if temp[1] != 'w':
                     word = temp[0]
-                    proc_sentence.append(word)           
-            for pos in range(len(proc_sentence) - 1):
-                single_npair.append((proc_sentence[pos], proc_sentence[pos + dist]))
+                    proc_sentence.append(word)
+            if dist >= 0:           
+                for pos in range(len(proc_sentence) - 1):
+                    if pos + dist < len(proc_sentence):
+                        single_npair.append((proc_sentence[pos], proc_sentence[pos + dist]))
+            else:
+                for pos in range(len(proc_sentence) - 1)[::-1]:
+                    if pos + dist >= 0:
+                        single_npair.append((proc_sentence[pos], proc_sentence[pos + dist]))
     return single_npair
 
+# nebor frequence calc
 def fre_calculate(curtext, dict):
     for pair in curtext:
         if not dict.__contains__(pair):
@@ -31,40 +38,57 @@ def fre_calculate(curtext, dict):
         else:
             dict[pair] += 1
 
-def filter_result(dict):
+def filter_result(dict, judge):
     filtresult = []
     sorted_res = sorted(dict.items(), key = lambda k: k[1])
-    # res.append([pair, num] for pair, num in dict_pair())
     for i in reversed(sorted_res):
-        if i[1] > 100:
+        # print(i[1], type(i[1]))
+        if i[1] > judge:
             filtresult.append(i)
     return filtresult
 
+"""
 def distance_frequence(curpair, step):
-    """
+    '''
     :type pair: tuple
-    :rtype: dict
-    """
+    '''
     pair = curpair[0]
     value = curpair[1]
     if not dict_res.__contains__(pair):
         dict_res.update({pair : [value]})
     elif step == 0:
-        return dict_res
+        return
     else:
-
+        for i in range(len(doc)):
+            tmp_pair = {}
+            step_res = text_process(rootdir + doc[i], step)
+            fre_calculate(step_res, tmp_pair)
+            if tmp_pair.__contains__(pair):
+                # dict_res[pair] += tmp_pair[pair]
+                dict_res[pair].append(tmp_pair[pair])
+            else:
+                # dict_res[pair] += 0
+                dict_res[pair].append(0)
+"""
 
 def main():
     # with open(resdir + '1_a.txt', 'w', encoding='gbk') as res:
-    doc = list(os.listdir(rootdir))     
     for i in doc:
         if i == '.DS_Store':
             doc.remove(i)
-    dict_pair = {}
-    for i in range(len(doc)):
-        fre_calculate(text_process(rootdir + doc[i], 1), dict_pair)
-    print(filter_result(dict_pair))
-    # print(type(res[0][0][0]), type(res[0][1]))
+    for step in range(-5, 6, 1):
+        for i in range(len(doc)):
+            fre_calculate(text_process(rootdir + doc[i], step), dict_pair)
+        one_res = filter_result(dict_pair, 100)
+        print(one_res)
+
+    """   
+    for i in dict_pair:
+        distance_frequence(i, 1)
+    """
+        
+    # print(dict_res)
+    
 
 if __name__ == '__main__':
     main()
