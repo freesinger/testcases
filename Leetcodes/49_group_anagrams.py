85d30148c5730fdfce320ef4fe6113bc5dd4b34f
freesinger <wzy545@vip.qq.com>
2019-05-16 16:11:15 +0800

    Create 49_group_anagrams.py

---
+++ Leetcodes/49_group_anagrams.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        idx_dict = defaultdict(list)
+        temp = [''.join(j) for j in [sorted(i) for i in strs]]
+        for idx, string in enumerate(temp):
+            idx_dict[string].append(idx)
+        return [list(strs[i] for i in j) for j in idx_dict.values()]
+
+t = Solution()
+# i = input()
+# ['', '']
+print(t.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
