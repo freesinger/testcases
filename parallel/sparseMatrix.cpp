93f1e79a69ecbf33b9c768c46f49b92ac3c4cede
freesinger <wzy545@vip.qq.com>
2019-06-14 00:55:41 +0800

    add CG first version

--- parallel/sparseMatrix.cpp
+++ parallel/sparseMatrix.cpp
@@ -5,8 +5,11 @@
 using namespace Eigen;
 using namespace std;
 
+#define MATRIXSIZE 10000
+
 typedef SparseMatrix<double> SpMat;
 typedef Triplet<double> T;
+ConjugateGradient<SpMat, Lower|Upper> cg;
 
 SpMat generateSparseMat(int size) {
     vector<T> tripleList;
@@ -14,22 +17,41 @@ SpMat generateSparseMat(int size) {
 
     for (int i = 0; i < size; i++)
         for (int j = 0; j < size; j++)
-            if ((i+j) == (size-1) || (i+j) == (size-6))
-                tripleList.push_back(T(i, j, rand()%10+1));
+            if (rand()%10 < 2) {
+                tripleList.push_back(T(i, j, 1));
+                tripleList.push_back(T(j, i, 1));
+            }
 
-    SpMat A(size, size);
-    A.setFromTriplets(tripleList.begin(), tripleList.end());
-    return A;
+    SpMat M(size, size);
+    // sort(tripleList.begin(), tripleList.end());
+    // tripleList.erase(unique(tripleList.begin(), tripleList.end()), tripleList.end());
+    M.setFromTriplets(tripleList.begin(), tripleList.end());
+    return M;
 }
 
 int main(int argc, const char **argv) {
-
+    VectorXd x = VectorXd::Random(100);
+    VectorXd b = VectorXd::Random(100);
+    SpMat A = generateSparseMat(100);
+    /* 
     // a strong restriction is that the storage orders must match
     SpMat mat = generateSparseMat(10);
     SpMat temp = SpMat(mat.transpose()) + mat;
     cout<<temp<<endl;
+    */
+    cout<<A<<endl;
+    cout<<b<<endl;
+    cout<<x<<endl;
 
-    
-    cout<<mat.transpose()*mat<<endl;
+    cg.compute(A);
+    x = cg.solve(b);
+    cout<<x<<endl;
+    cout<<"#iteration: "<<cg.iterations()<<endl;
+    cout<<"estimated error: "<<cg.error()<<endl;
+
+    x = cg.solve(b);
+
+    cout<<x<<endl;
+    // cout<<A.transpose()*A<<endl;
     return 0;
 }
