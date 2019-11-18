2dc6cdccfc8009908522c9e53c5775b32e2d87e7
freesinger <wzy545@vip.qq.com>
2019-11-19 00:06:59 +0800

    Update

---
+++ Leetcodes/1190.reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1190 lang=java
+ *
+ * [1190] Reverse Substrings Between Each Pair of Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseParentheses(String s) {
+        
+    }
+}
+// @lc code=end
+
---
+++ Leetcodes/1190.反转每对括号间的子串.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+/*
+ * @lc app=leetcode.cn id=1190 lang=java
+ *
+ * [1190] 反转每对括号间的子串
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseParentheses(String s) {
+        Stack<String> stack = new Stack<>();
+        stack.push("");
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push("");
+            } else if (s.charAt(i) == ')') {
+                String reverseTop = reverseString(stack.pop());
+                stack.push(stack.pop() + reverseTop);  
+            } else {
+                String top = stack.pop();
+                stack.push(top + s.charAt(i));
+            }
+        }
+        return stack.pop();
+    }
+
+    private String reverseString(String s) {
+        String res = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            res += s.charAt(i);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
